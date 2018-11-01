abce85cf803cbed2330a610298e59b08f17e57b6
ellreka <kozakana333@gmail.com>
2018-11-01 11:21:18 +0900

    a

--- static/index.js
+++ static/index.js
@@ -2,13 +2,14 @@ const app = new Vue({
   el: '#app',
   delimiters: ['[[', ']]'],
   data:{
-    num:null,
-    tags:null
+    tags:'ALL',
+    num:1
 
   },
   methods:{
     open: function() {
-      axios.post('/open'),{tags:this.tags,num:this.num}
+      console.log(this.tags)
+      axios.post('/open',{tags:this.tags,num:this.num})
         .then(response => {
           console.log(response)
         })
