33151abf9aea95bcb894c64f59ce629942f1cc2f
ellreka <kozakana333@gmail.com>
2018-11-03 01:09:00 +0900

    edit about

--- app.py
+++ app.py
@@ -20,25 +20,11 @@ redirect_uri = 'https://random-pocket-opener.herokuapp.com/callback'
 @app.route('/',methods=['GET','POST'])
 def index():
   access_token = request.cookies.get('access_token')
-  if not access_token == '':
-    print(access_token)
-    path = "bookmark.json"
-    print(os.path.exists(path))
-    if os.path.exists(path):
-      with open("bookmark.json", 'r') as f:
-        l = json.load(f)
-      tags = l[0]
-      updated = '2018/10/17'
-      article_len = len(l[1])
-      return render_template('index.html',tags=tags,article_len=article_len,updated=updated)
-    else:
-      # with open("bookmark.json", 'w') as f:
-      #   print(f)
-      return render_template('index.html')
+  print(access_token)
+  if not access_token == None:
+    return render_template('index.html')
   else:
     return render_template('login.html')
-  # return render_template('login.html')
-  # return render_template('index.html')
 
   
 
@@ -59,10 +45,8 @@ def login():
 def logout():
   if request.method == 'POST':
     print('ログアウト')
-    resp = make_response(redirect(url_for('index')))
-    resp.set_cookie('access_token', '')
-    os.remove("bookmark.json")
-    return resp
+    # resp = make_response(redirect(url_for('index')))
+    # resp.set_cookie('access_token', '')
     return redirect(url_for('index'))
   else:
     return redirect(url_for('index'))
@@ -83,80 +67,13 @@ def callback():
 @app.route('/open', methods=['GET','POST'])
 def pick():
   if request.method == 'POST':
-    with open("bookmark.json", 'r') as f:
-      l = json.load(f)
-    all_list = []
     print(request.json)
-    if request.json['tags'] == 'ALL':
-      for a in l[1]:
-        all_list.append(a)
-      ramdom_list = random.sample(all_list, int(request.json['num']))
-    elif request.json['tags'] == 'NoTag':
-      print('NoTag')
-      for a in l[1]:
-        if not a['tag']:
-          print(a['tag'])
-          all_list.append(a)
-      ramdom_list = random.sample(all_list, int(request.json['num']))
-    else:
-      print(request.json['tags'])
-      for a in l[1]:
-        for b in a['tag']:
-          if b == request.json['tags']:
-            all_list.append(a)
-      print(all_list)
-      print(request.json['num'])
-      ramdom_list = random.sample(all_list, int(request.json['num']))
+    ramdom_list = random.sample(request.json["article"], int(request.json["num"]))
     return jsonify(ramdom_list)
     return redirect(url_for('index'))
   else:
     return redirect(url_for('index'))
 
-
-
-    # access_token = request.cookies.get('access_token')
-    # timestamp = request.cookies.get('timestamp')
-    # payload = {'consumer_key': consumer_key,'access_token': access_token ,'contentType':'article','sort':'newest','detailType':'complete','count':'2','since':timestamp}
-    # r=requests.post('https://getpocket.com/v3/get',data=payload)
-    # new_pocket = r.json()
-    # print(new_pocket)
-    # with open("assets/bookmark.json", 'r') as f:
-    #   l = json.load(f)
-    #   l.append(new_pocket)
-    #   with open("assets/bookmark.json", 'w') as f:
-    #     json.dump(l, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
-    # return jsonify("https://tweetdeck.twitter.com/")  
-
-
-
-
-  # if request.method == 'POST':
-
-  #   # payload = {'consumer_key': consumer_key,'access_token': access_token , 'state':'all','sort':'newest','detailType':'complete'}
-  #   # r=requests.post('https://getpocket.com/v3/get',data=payload)
-  #   # l = r.json()
-  #   # values = random.sample([a[1] for a in list(l['list'].items())],int(3))
-  #   f = open('assets/bookmark.json','r')
-  #   l = json.load(f)
-  #   # すべてとそれ以外で場合分け
-  #   if request.json['tag'] == '全て':
-  #     print('aaaaa')
-  #     values = random.sample([a[1] for a in list(l['list'].items())],int(request.json['number']))
-  #     return jsonify(values)
-  #   else:
-  #     arr = []
-  #     print('eeeeee')
-  #     for k in l['list'].items():
-  #       if request.json['tag'] in k[1]['tags']:
-  #         if k[1]['tags'][request.json['tag']]['tag'] == request.json['tag']:
-  #           print(k[1]['tags'])
-  #           arr.append(k[1])        
-  #         else:
-  #           print('aeuhetase')
-  #   values = random.sample(arr,int(request.json['number']))
-  #   return jsonify(values)
-  # else:
-  #   return redirect(url_for('index'))
 
 @app.route('/refresh', methods=['POST'])
 def refresh():
@@ -164,15 +81,10 @@ def refresh():
   print(access_token)
   payload = {'consumer_key': consumer_key,'access_token': access_token ,'state':'all','contentType':'article','sort':'newest','detailType':'complete'}
   r=requests.post('https://getpocket.com/v3/get',data=payload)
-
-
   all_pocket = r.json()
-  print(all_pocket)
-
   simple_pocket_list = []
   page_data_list = []
   tags_list = []
-
   for a in all_pocket['list'].items():
     l = {
       "title":a[1]['resolved_title'],
@@ -185,10 +97,7 @@ def refresh():
   tags_list = list(set(tags_list))
   simple_pocket_list.append(tags_list)
   simple_pocket_list.append(page_data_list)
-  print(simple_pocket_list)
-
-  with open("bookmark.json", 'w') as f:
-    json.dump(simple_pocket_list, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+  return jsonify(page_data_list,tags_list)
   return redirect(url_for('index'))
 
 
--- static/index.js
+++ static/index.js
@@ -1,34 +1,100 @@
-// const app = new Vue({
-//   el: '#app',
-//   delimiters: ['[[', ']]'],
-//   data:{
-//     formData:{
-//       tags:'',
-//       num:''
-//     }
-//   },
-//   methods:{
-//     open: function() {
-//       console.log(this.tags)
-//       axios.post('/open',this.formData)
-//         .then(response => {
-//           console.log(response)
-//         })
-//       .catch(error => {
-//         console.log(error);
-//       })
-//     },
-//     refresh: function(){
-//       axios.post('/refresh')
-//         .then(response => {
-//           console.log(response.data)
-//         })
-//       .catch(error => {
-//         console.log(error);
-//       })
-//       getCookie()
-//     }
-//   }
-// })
+const db = new Dexie("MyDatabase");
 
 
+
+const app = new Vue({
+  el: '#app',
+  delimiters: ['[[', ']]'],
+  data:{
+    formData:{
+      tags:'ALL',
+      num:1
+    },
+    allTag:[],
+    history:[]
+  },
+  methods:{
+    pick:async function ()  {
+      var arr_articles = []
+      if(this.formData["tags"] == "ALL"){
+        await db.articles.each(function(t){
+          arr_articles.push(t)
+          console.log(t)
+        })
+      }else if(this.formData["tags"] == "NoTag"){
+        await db.articles.where("tag").equals("").each(function(t){
+          arr_articles.push(t)
+          console.log(t)
+        })
+      }else{
+        var regex = RegExp(this.formData["tags"])
+        await db.articles.filter(article => regex.test(article.tag)).each(function(t){
+          arr_articles.push(t)
+          console.log(t)
+        })
+      }
+      console.log(db)
+      console.log(arr_articles)
+      axios.post('/open',{"article":arr_articles,"num":this.formData["num"]})
+        .then(response => {
+          console.log(response.data)
+          for(let i in response.data){
+            console.log(response.data[i]['url'])
+            this.history.push(response.data[i])
+            window.open(response.data[i]['url'])
+          }
+        })
+      .catch(error => {
+        console.log(error);
+      })
+    },
+    refresh: function(){
+      axios.post('/refresh')
+        .then(response => {
+          console.log(response.data)
+
+          console.log(db)
+          db.articles.clear()
+          db.tags.clear()
+
+          response.data[0].forEach((val) => {
+            db.articles.add({title: val.title,url: val.url,tag: val.tag.join()})
+          })
+          response.data[1].forEach((val) => {
+            db.tags.add({tag:val})
+          })
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    },
+    logout: function(){
+      Dexie.delete('MyDatabase');
+      Dexie.delete('__dbnames');
+      document.cookie = "access_token=; max-age=0";
+      axios.post('/logout')
+        .then(response => {
+          console.log(response.data)
+
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  }
+})
+
+const getTag = (async() => {
+  var arr_tag = []
+  db.version(1).stores({
+    articles: "title, url, tag",
+    tags: "tag"
+  });
+  await db.tags.each(function(t){
+    arr_tag.push(t.tag)
+    console.log(t)
+  })
+  app.allTag = arr_tag
+  console.log(app.allTag)
+})
+getTag()
