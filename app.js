476800979cb521086867084c205a6fe1d91c0560
ellreka <kozakana333@gmail.com>
2019-07-12 22:17:13 +0900

    a

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // app.set('view engine', 'pug');
-app.engin('pug', require('pug').__express)
+app.engine('pug', require('pug').__express)
 
 app.set('trust proxy', 1);
 
