8eeb4a7cee196a0220809a36a760f96af141078e
ellreka <kozakana333@gmail.com>
2019-07-12 22:12:40 +0900

    o

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
+app.engin('pug', require('pug').__express)
 
 app.set('trust proxy', 1);
 
