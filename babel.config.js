454cdcaaada0f5035ababfa3ff2a7f075b03a42e
ellreka <kozakana333@gmail.com>
2019-10-10 02:21:17 +0900

    update:babel

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [["@babel/preset-env", {
+    "useBuiltIns": "usage",
+    "corejs": "3"
+  }],
+    ["@babel/preset-react"]
+  ];
+
+  return {
+    presets,
+  };
+}
